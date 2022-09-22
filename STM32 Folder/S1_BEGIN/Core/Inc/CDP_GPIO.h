95c31e42859e0b9411c6dd27e52a9d2762a58331
CDXRACI <=>
2022-09-22 13:12:50 +0700

    F2

---
+++ STM32 Folder/S1_BEGIN/Core/Inc/CDP_GPIO.h
@@ -0,0 +1,17 @@
+
+
+#ifndef __CDP_GPIO_H
+#define __CDP_GPIO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void GPIO_BLINK();
+void GPIO_WRITE_PIN();
+void GPIO_READ_PIN();
+
+#ifdef __cplusplus
+}
+#endif
+#endif
